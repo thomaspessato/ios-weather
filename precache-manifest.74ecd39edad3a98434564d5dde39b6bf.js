651e892b5685cf12e251810f60d39cb6ab54b821
Thomas Pessato <thomas@clusterdesign.com.br>
2019-06-27 19:22:05 +1000

    Updates

---
+++ precache-manifest.74ecd39edad3a98434564d5dde39b6bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1ea22ae98a23336095fb2bce2d860bc",
+    "url": "/ios-weather/index.html"
+  },
+  {
+    "revision": "5bf89da926103cae4fff",
+    "url": "/ios-weather/static/css/main.6d10a0be.chunk.css"
+  },
+  {
+    "revision": "2790c44532533aa1f119",
+    "url": "/ios-weather/static/js/2.2668f2aa.chunk.js"
+  },
+  {
+    "revision": "5bf89da926103cae4fff",
+    "url": "/ios-weather/static/js/main.902d9ef0.chunk.js"
+  },
+  {
+    "revision": "8784145b13209eb6e4d1",
+    "url": "/ios-weather/static/js/runtime~main.379efb22.js"
+  }
+]);
